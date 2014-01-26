c936b51eb14f80d0b080f36c26c9da176740af22
mojoaxel <alex@wunschik.net>
2014-01-26 19:59:16 +0100

    fixed router

--- router.js
+++ router.js
@@ -8,13 +8,13 @@ app.get('/', function(req, res) {
 	req.pipe(request(url)).pipe(res);
 });
 
-app.get('/netmon', function(req, res) {
+app.get('/netmon/:xxx', function(req, res) {
 	url = 'http://127.0.0.1:9001' + req.url;
 	console.log(url);
 	req.pipe(request(url)).pipe(res);
 });
 
-app.get('/api', function(req, res) {
+app.get('/api/:xxx', function(req, res) {
 	url = 'http://127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite' + req.url;
 	console.log(url);
 	req.pipe(request(url)).pipe(res);
