afd2665f855f7291b007e90aa795e4d57b79ca3b
mojoaxel <alex@wunschik.net>
2014-01-26 20:01:53 +0100

    only route the webUI

--- router.js
+++ router.js
@@ -8,16 +8,4 @@ app.get('/', function(req, res) {
 	req.pipe(request(url)).pipe(res);
 });
 
-app.get('/netmon/:xxx', function(req, res) {
-	url = 'http://127.0.0.1:9001' + req.url;
-	console.log(url);
-	req.pipe(request(url)).pipe(res);
-});
-
-app.get('/api/:xxx', function(req, res) {
-	url = 'http://127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite' + req.url;
-	console.log(url);
-	req.pipe(request(url)).pipe(res);
-});
-
 app.listen(80);
