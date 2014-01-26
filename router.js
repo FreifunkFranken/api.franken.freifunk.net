9dd23ccfc01d8dc48749ebbe7388251ca4736aad
mojoaxel <alex@wunschik.net>
2014-01-26 19:49:12 +0100

    updated router.js

--- router.js
+++ router.js
@@ -2,22 +2,22 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
-app.use('/', function(req, res) {
-        url = 'http://127.0.0.1:9000' + req.url;
-        console.log(url);
-        req.pipe(request(url)).pipe(res);
+app.get('/', function(req, res) {
+	url = 'http://127.0.0.1:9000' + req.url;
+	console.log(url);
+	req.pipe(request(url)).pipe(res);
 });
 
-app.use('/api', function(req, res) {
-        url = 'http://127.0.0.1:9001' + req.url;
-        console.log(url);
-        req.pipe(request(url)).pipe(res);
+app.get('/netmon', function(req, res) {
+	url = 'http://127.0.0.1:9001' + req.url;
+	console.log(url);
+	req.pipe(request(url)).pipe(res);
 });
 
-app.use('/api', function(req, res) {
-        url = 'http://127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite' + req.url;
-        console.log(url);
-        req.pipe(request(url)).pipe(res);
+app.get('/api', function(req, res) {
+	url = 'http://127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite' + req.url;
+	console.log(url);
+	req.pipe(request(url)).pipe(res);
 });
 
 app.listen(80);
