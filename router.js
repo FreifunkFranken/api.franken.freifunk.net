7a3defce16cda70183aa738caed48f32b95eeb46
mojoaxel <alex@wunschik.net>
2014-01-26 19:30:58 +0100

    removed settings

--- router.js
+++ router.js
@@ -1,17 +1,13 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
 
-var settings = {
-	domain: "libremap.freifunk-franken.de",
-	libremapWebUi_port: 9000,
-	frankenApi_port: 9001
-}
+var domain = "libremap.freifunk-franken.de";
 
 httpProxy.createServer({
 	hostnameOnly: true,
 	router: {
-		settings.domain + '/' 			: '127.0.0.1:' + settings.libremapWebUi_port,
-		settings.domain + '/netmon/' 	: '127.0.0.1:' + settings.frankenApi_port,
-		settings.domain + '/api/' 		: '127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite'
+		domain + '/' 			: '127.0.0.1:9000',
+		domain + '/netmon/' 	: '127.0.0.1:9001',
+		domain + '/api/' 		: '127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite'
 	}
 }).listen(80);
