d2a860bb2b8b8d6d9b886c4e14035eb993ac7009
mojoaxel <alex@wunschik.net>
2014-01-26 19:46:38 +0100

    fixed router.js

--- router.js
+++ router.js
@@ -1,11 +1,23 @@
-var http = require('http');
-var httpProxy = require('http-proxy');
+var express = require('express');
+var request = require('request');
+var app = express();
 
-httpProxy.createServer({
-	hostnameOnly: true,
-	router: {
-		'libremap.freifunk-franken.de/' 		: '127.0.0.1:9000',
-		'libremap.freifunk-franken.de/netmon/' 	: '127.0.0.1:9001',
-		'libremap.freifunk-franken.de/api/' 	: '127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite'
-	}
-}).listen(80);
+app.use('/', function(req, res) {
+        url = 'http://127.0.0.1:9000' + req.url;
+        console.log(url);
+        req.pipe(request(url)).pipe(res);
+});
+
+app.use('/api', function(req, res) {
+        url = 'http://127.0.0.1:9001' + req.url;
+        console.log(url);
+        req.pipe(request(url)).pipe(res);
+});
+
+app.use('/api', function(req, res) {
+        url = 'http://127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite' + req.url;
+        console.log(url);
+        req.pipe(request(url)).pipe(res);
+});
+
+app.listen(80);
