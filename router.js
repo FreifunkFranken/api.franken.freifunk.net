8a7f2d16404a13e60c89c7eac5197bf7e587601a
mojoaxel <alex@wunschik.net>
2014-01-26 19:31:54 +0100

    simplyfied

--- router.js
+++ router.js
@@ -1,13 +1,11 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
 
-var domain = "libremap.freifunk-franken.de";
-
 httpProxy.createServer({
 	hostnameOnly: true,
 	router: {
-		domain + '/' 			: '127.0.0.1:9000',
-		domain + '/netmon/' 	: '127.0.0.1:9001',
-		domain + '/api/' 		: '127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite'
+		'libremap.freifunk-franken.de/' 		: '127.0.0.1:9000',
+		'libremap.freifunk-franken.de/netmon/' 	: '127.0.0.1:9001',
+		'libremap.freifunk-franken.de/api/' 	: '127.0.0.1:5984/libremap-dev/_design/libremap-api/_rewrite'
 	}
 }).listen(80);
